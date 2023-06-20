f3e605c9cc00a9985c5aaf9dda186bfcadb52565
Meshack Korir <meshackkorir100@gmail.com>
2023-06-20 09:59:50 +0300

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cheese.create!([{name: 'Cheddar', price: 3, is_best_seller: true}, {name: 'Pepper Jack', price: 4, is_best_seller: true}, {name: 'Limburger', price: 8, is_best_seller: true}])
